718527fd05f3c0029eb93ff59ec46611850b402e
Randall Coding <randallcoding@protonmail.com>
2023-10-31 13:24:27 +0700

    Updates entrypoint with new config files and environment variables

--- containerfs/start.sh
+++ containerfs/start.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-# Install cs2
+# install
 echo "Installing cs2"
-$STEAMCMD_DIR/steamcmd.sh +force_install_dir $APP_DIR +login $STEAM_USER $STEAM_PASS +app_update 730 validate +quit
+$STEAMCMD_DIR/steamcmd.sh +@sSteamCmdForcePlatformType linux +force_install_dir $APP_DIR +login $STEAM_USER $STEAM_PASS +app_update 730 +quit
 
-cat <<EOF > ./server.bat
-$STEAMCMD_DIR/steamcmd.sh +force_install_dir $APP_DIR +login $STEAM_USER $STEAM_PASS +app_update 730 validate +quit
-cd server/game/bin/ubuntu32
-start /wait cs2 -dedicated -usercon -console -secure -dev +game_type 0 +game_mode 1 +sv_logfile 1 -serverlogging +sv_setsteamaccount $SERVER_ID +map de_inferno +exec server.cfg
+# copy config files
+mv $STEAM_DIR/server.cfg $APP_DIR/game/csgo/cfg/server.cfg
+mv $STEAM_DIR/gamemode_casual_server.cfg $APP_DIR/game/csgo/cfg/gamemode_casual_server.cfg
+mv $STEAM_DIR/gamemode_competitive_server.cfg $APP_DIR/game/csgo/cfg/gamemode_competitive_server.cfg
+mv $STEAM_DIR/mapcycle.txt $APP_DIR/game/csgo/mapcycle.txt
+
+# Check if SERVER_PASSWORD is set and if so, append it to server.cfg
+if [[ -n "$SERVER_PASSWORD" ]]; then
+    echo "sv_password \"$SERVER_PASSWORD\"" >> $APP_DIR/game/csgo/cfg/gamemode_casual_server.cfg
+fi
+
+# Check if RCON_PASSWORD is set and if so, append it to server.cfg
+if [[ -n "$RCON_PASSWORD" ]]; then
+    echo "rcon_password \"$RCON_PASSWORD\"" >> $APP_DIR/game/csgo/cfg/gamemode_casual_server.cfg
+fi
+
+# create run script
+cat <<EOF > $APP_DIR/server.sh
+$STEAMCMD_DIR/steamcmd.sh +force_install_dir $APP_DIR +login $STEAM_USER $STEAM_PASS +app_update 730 +quit
+cd $APP_DIR/game/bin/linuxsteamrt64/
+./cs2 -dedicated -usercon -console -secure -dev +game_type 0 +game_mode ${GAME_MODE:-0} +sv_logfile 1 -serverlogging +sv_setsteamaccount $SERVER_TOKEN +map de_dust2 +exec $APP_DIR/game/csgo/cfg/server.cfg
 EOF
 
-mv $APP_DIR/../server.cfg $APP_DIR/server/game/cs2/cfg/server.cfg
+# run server.sh
+chmod +x $APP_DIR/server.sh
+echo "Starting server with server.sh"
+$APP_DIR/server.sh
